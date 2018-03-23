bfcbe14ea51f27bb2c948a41427265cc54c7b564
rekkles11 <rekklesss11@gmail.com>
2018-03-23 18:49:43 +0800

    first commit

---
+++ app/src/main/java/wangbin/graduation/com/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package wangbin.graduation.com.myapplication;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
